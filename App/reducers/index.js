c4f6177b5a04ad37087da27ddadb0986f5cfdb72
Eric Chang <eric61213@gmail.com>
2018-01-20 19:46:04 -0800

    udpated filters.js -> filter.js

--- App/reducers/index.js
+++ App/reducers/index.js
@@ -2,7 +2,7 @@
 
 import { combineReducers } from 'redux';
 import taskReducer from './task';
-import filterReducer from './filters';
+import filterReducer from './filter';
 
 export default combineReducers({
   tasks: taskReducer,
